401aa5590009dd5d88c278d6579dcf4279b465b5
JnsWnk <wnkjns@gmail.com>
2023-04-09 19:50:49 +0200

    options for vercel

--- index.js
+++ index.js
@@ -24,18 +24,6 @@ async function getBrowserInstance() {
 }
 
 app.get("/api", async (req, res) => {
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
-
   try {
     let browser = await getBrowserInstance();
 
@@ -51,3 +39,5 @@ app.get("/api", async (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
 });
+
+module.exports = app;
