e8d8c2a4c45169261e259b555c6954a35e788f90
JnsWnk <wnkjns@gmail.com>
2023-05-08 23:51:38 +0200

    Starting to implement proxy test.

---
+++ utils/proxy.js
@@ -0,0 +1,30 @@
+const axios = require("axios");
+
+const testProxy = async (proxy) => {
+  const url = "https://www.google.com/";
+  const timeout = 5000; // Adjust the timeout value as per your requirements
+
+  try {
+    const response = await axios.get(url, {
+      proxy: {
+        host: proxy.host,
+        port: proxy.port,
+      },
+      timeout,
+    });
+
+    if (response.status === 200) {
+      console.log(`Proxy ${proxy.host}:${proxy.port} is working.`);
+    } else {
+      console.log(
+        `Proxy ${proxy.host}:${proxy.port} returned non-200 status code: ${response.status}`
+      );
+    }
+  } catch (error) {
+    console.log(`Proxy ${proxy.host}:${proxy.port} is not working.`);
+  }
+};
+
+testProxies = async (proxies) => {};
+
+module.exports = { testProxy, testProxies };
